dfd6c760db413e7ac9666106ec073e6c99f6234d
toml@q4inc.com <tomluong@live.com>
2017-05-12 23:37:52 -0400

    add module Client and its components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    '../../node_modules/bootstrap/dist/css/bootstrap.min.css',
+    './app.component.css'
+    ]
 })
 export class AppComponent {
-  title = 'app works!';
+  
 }
---
+++ src/app/client/client.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClientComponent } from './client.component';
+
+
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ClientComponent ]
+})
+export class ClientModule { }
